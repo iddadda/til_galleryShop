80fa9d0bd412112a41b2dddd652b76503c8d0fe7
iddadda <iddadda241@gmail.com>
2025-07-08 16:07:18 +0900

    [feat]: pinia 처리

--- src/services/accountService.js
+++ src/services/accountService.js
@@ -1,6 +1,14 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:8080/api/v1";
+axios.defaults.baseURL = "http://localhost:8080/api/v1/account";
 
 export const join = (args) => {
-  return axios.post("/account/join", args).catch((e) => e.response);
+  return axios.post("/join", args).catch((e) => null);
+};
+
+export const login = (args) => {
+  return axios.post("/login", args).catch((e) => null);
+};
+
+export const check = () => {
+  return axios.get("/check").catch((e) => null);
 };
